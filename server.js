ec7b4c072bdd6462291dcbf24a2637311d8a99db
Marcus Vítor <marcusvitormorais032@gmail.com>
2025-12-29 14:04:03 -0300

    ajuistes ate o prisma

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 import express from 'express'
+import { PrismaClient, PrismaMongoDBAdapter } from './generated/prisma'
+import { MongoClient } from 'mongodb'
 
 const app = express()
 app.use(express.json())
 
-const users = []
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+await mongoClient.connect()
+const adapter = new PrismaMongoDBAdapter(mongoClient)
+const prisma = new PrismaClient({ adapter })
 
-app.post('/usuarios', (req, res) => {
-
-    users.push(req.body)
-
-    res.status(201).json(req.body)
+app.post('/usuarios', async (req, res) => {
+    const user = await prisma.user.create({ data: req.body })
+    res.status(201).json(user)
 })
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 }) 
 
